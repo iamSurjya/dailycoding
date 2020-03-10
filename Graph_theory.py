f5d0dd42fd2e6e1007ec9c06c8cb5a4014032012
Suraj Choudhary <36595579+iamSurjya@users.noreply.github.com>
2020-03-10 21:27:04 +0530

    Update and rename Day 63 Graph_theory.py to Graph_theory.py

--- Graph_theory.py
+++ Graph_theory.py
@@ -75,3 +75,44 @@ def buildCityGraph(graphType):
 
 g=buildCityGraph(Digraph)
 print(g)
+
+def printPath(path):
+    """Assumes path is a list of nodes"""
+    result = ''
+    for i in range(len(path)):
+        result = result + str(path[i])
+        if i != len(path) - 1:
+            result = result + '->'
+    return result
+
+def DFS(graph,start,end,path,shortest,toPrint=False):
+    path=path+[start]
+    if toPrint:
+        print('Current DFS path',printPath(path))
+    if start==end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path:
+            if shortest==None or len(path)<len(shortest):
+                newPath=DFS(graph,node,end,path,shortest,toPrint)
+                if newPath!=None:
+                    shortest=newPath
+        elif toPrint:
+            print('Already visited',node)
+    return shortest
+
+def shortestPath(graph,start,end,toPrint=False):
+    return DFS(graph,start,end,[],None,toPrint)
+
+
+def testSP(source, destination):
+    g = buildCityGraph(Digraph)
+    sp = shortestPath(g, g.getNode(source), g.getNode(destination),toPrint = True)
+    if sp != None:
+        print('Shortest path from', source, 'to',
+              destination, 'is', printPath(sp))
+    else:
+        print('There is no path from', source, 'to', destination)
+
+testSP('Chicago', 'Boston')
+print()
