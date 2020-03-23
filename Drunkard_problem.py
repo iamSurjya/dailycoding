07efd2812cbe27296e1a8aab6c86b7bb31dbb237
Suraj Choudhary <36595579+iamSurjya@users.noreply.github.com>
2020-03-23 22:23:43 +0530

    Update Drunkard_problem.py

--- Drunkard_problem.py
+++ Drunkard_problem.py
@@ -1,29 +1,42 @@
+import random
 class Location(object):
     def __init__(self,X,Y):
         self.X=X
         self.Y=Y
-
     def move(self,deltaX,deltaY):
-        return Location(deltaX,deltaY)
-
+        return Location(self.X+deltaX,self.Y+deltaY)
     def getX(self):
         return self.X
-
     def getY(self):
         return self.Y
-
     def distFrom(self,other):
         xDist=self.X-other.getX()
         yDist=self.Y-other.getY()
         return(xDist**2 + yDist**2)**0.5
-        
     def __str__(self):
         return '<'+ str(self.X)+','+str(self.Y)+'>'
 
-class Drunk(object):
-    def __init(self,name=None):
-        self.name=name
+class Feild(object):
+    def __init__(self):
+        self.drunks={}
+    def addDrunk(self,drunk,loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate Error')
+        else :
+            self.drunks[drunk]=loc
+    def moveDrunk(self,drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in Feild')
+        xDist,yDist=drunk.takeStep()
+        self.drunks[drunk]=self.drunks[drunk].move(xDist,yDist)
+    def getLoc(self,drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in Feild')
+        return self.drunks[drunk]
 
+class Drunk(object):
+    def __init__(self,name=None):
+        self.name=name
     def __str__(self):
         if self!=None:
             return self.name
@@ -41,29 +54,31 @@ class NorthMoving(Drunk):
         stepChoices=[(0.0,1.1),(0.0,0.9),(1.0,0.0),(-1.0,0.0)]
         return random.choice(stepChoices)
 
-class Feild(object):
-    def __init__(self):
-        self.drunks={}
 
-    def addDrunk(self,Drunk,loc):
-        if drunk in self.drunks:
-            raise ValueError('Duplicate Error')
-        else :
-            self.drunks[drunk]=loc
 
-    def getLoc(self,drunk):
-        if drunk not in self.drunks:
-            raise ValueError('Drunk not in Feild')
-        return self.drunks[drunk]
+def walk(f,d,numSteps): #f:Feild,d:drunk, numSteps:Number of steps taken by drunks
+    start=f.getLoc(d)
+    for s in range(numSteps):
+        f.moveDrunk(d)
+    return start.distFrom(f.getLoc(d))
 
-    def moveDrunk(self,drunk):
-        if drunk not in self.drunks:
-            raise ValueError('Drunk not in Feild')
-        xDist,yDist=drunk.takeStep()
-        self.drunks[drunk]=self.drunks[drunk].move(xDist,yDist)
 
-    def walk(f,d,numSteps): #f:Feild,d:drunk, numSteps:Number of steps taken by drunks
-        start=f.getLoc(d)
-        for s in range(numSteps):
-            f.moveDrunk(d)
-        return start.distFrom(f.getLoc(d))
+def simWalks(numSteps,numTrials,dClass):
+    Homer=dClass('Homer')
+    origin=Location(0,0)
+    distance=[]
+    for t in range(numTrials):
+        f=Feild()
+        f.addDrunk(Homer,origin)
+        distance.append(round(walk(f,Homer,numSteps),1))
+    return distance
+
+def drunkTest(Walklengths,numTrials,dClass):
+    for numSteps in Walklengths:
+        distance=simWalks(numSteps,numTrials,dClass)
+        print(dClass.__name__,'random walk of ',numSteps,'steps')
+        print('Mean =',round(sum(distance)/len(distance),4))
+        print('Max= ',max(distance),'Min= ',min(distance))
+
+random.seed(0)
+drunkTest((10,100,1000),100,UsualDrunk)
